7a98005cedd39a42e9199c6622fd17a22effdfce
Ashan Kavinda <bandaralamak.23@uom.lk>
2025-10-07 22:53:37 +0530

    done admin part

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -6,5 +6,12 @@ const router = express.Router();
 
 
 router.post('/add', adminController.createAdmin);
+router.get('/viewAll', adminController.viewAllAdmins);
+router.get('/view/:id', adminController.viewAdminById);
+router.get('/view/:username', adminController.viewAdminByUsername);
+router.get('/view/:email', adminController.viewAdminByEmail);
+router.get('/view/:address', adminController.viewAdminByAddress);
+router.patch('/update/:id', adminController.updateAdmin);
+router.delete('/delete/:id', adminController.deleteAdmin);
 
 module.exports = router;
