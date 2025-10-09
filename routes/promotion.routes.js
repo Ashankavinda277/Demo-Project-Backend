953c43cb2c047e31698fc800b1e59379c2557d8a
Asiri Harischandra <168220816+AsiriHarischandra@users.noreply.github.com>
2025-10-09 14:53:53 +0530

    error correction

--- routes/promotion.routes.js
+++ routes/promotion.routes.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 
 router.post('/add', promotionController.addPromotion);
-router.get('/get', promotionController.getPromotions);
-router.get('/get/:id', promotionController.getPromotionsbyId);
-router.get('/get/name/:name', promotionController.getPromotionsbyName);
-router.get('/get/type/:type', promotionController.getPromotionsbyType);
-router.get('/get/startDate/:date', promotionController.getPromotionsbyStartDate);
-router.get('/get/endDate/:date', promotionController.getPromotionsbyEndDate);
+router.get('/get', promotionController.getPromotion);
+router.get('/get/:id', promotionController.getPromotionbyId);
+router.get('/get/name/:name', promotionController.getPromotionbyName);
+router.get('/get/type/:type', promotionController.getPromotionbyType);
+router.get('/get/startDate/:date', promotionController.getPromotionbyStartDate);
+router.get('/get/endDate/:date', promotionController.getPromotionbyEndDate);
 router.put('/replace/:id', promotionController.replacePromotion);
 router.patch('/update/:id', promotionController.updatePromotion);
 router.delete('/delete/:id', promotionController.deletePromotion);
