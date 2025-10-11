ca3b878dcfecb0e89091050cb884f91e95df4540
Asiri Harischandra <168220816+AsiriHarischandra@users.noreply.github.com>
2025-10-12 03:38:31 +0530

    backend api connect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connect = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,11 @@ const adminRoute = require('./routes/admin.route');
 const customerRoute = require('./routes/costomer.route');
 const productRoute = require('./routes/product.route');
 const promotionRoute = require('./routes/promotion.routes');
+
+app.use(cors({
+    origin: 'http://localhost:5173', // Your Vite frontend URL
+    credentials: true
+}));
 
 app.use('/api/admin', adminRoute);
 app.use('/api/customer', customerRoute);
