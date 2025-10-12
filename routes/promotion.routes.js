bd990f7a714362d435ca71446fbacbef927f1593
Asiri Harischandra <168220816+AsiriHarischandra@users.noreply.github.com>
2025-10-12 15:14:13 +0530

    befo merger

--- routes/promotion.routes.js
+++ routes/promotion.routes.js
@@ -7,10 +7,10 @@ router.post('/add', promotionController.addPromotion);
 router.get('/get', promotionController.getPromotion);
 router.get('/get/:id', promotionController.getPromotionbyId);
 router.get('/get/name/:name', promotionController.getPromotionbyName);
-router.get('/get/type/:type', promotionController.getPromotionbyType);
-router.get('/get/startDate/:date', promotionController.getPromotionbyStartDate);
+// router.get('/get/type/:type', promotionController.getPromotionbyType);
+// router.get('/get/startDate/:date', promotionController.getPromotionbyStartDate);
 router.get('/get/endDate/:date', promotionController.getPromotionbyEndDate);
-router.put('/replace/:id', promotionController.replacePromotion);
+// router.put('/replace/:id', promotionController.replacePromotion);
 router.patch('/update/:id', promotionController.updatePromotion);
 router.delete('/delete/:id', promotionController.deletePromotion);
 
