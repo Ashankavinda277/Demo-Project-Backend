254f20e1116298cf4a4ec9cec715f1ffe12bcfb6
Asiri Harischandra <168220816+AsiriHarischandra@users.noreply.github.com>
2025-10-13 04:43:44 +0530

    aaa

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,6 +2,7 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 
+
 // Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
--- models/promotion.model.js
+++ models/promotion.model.js
@@ -1,16 +1,17 @@
+
 const mongoose = require('mongoose');
 const db = mongoose.connection.useDb('cake-shop');
 
 const promotionSchema = new mongoose.Schema({
-    Promotion_Name:{type: String, required: true},
+    Promotion_Name: { type: String, required: true },
     // Promotion_Type:{type:String, required:true},
-    Discount_Price:{type:Number, required:true},
+    Discount_Price: { type: Number, required: true },
     // Start_Date:{type:Date, required:true},
-    End_Date:{type:Date, required:true},
-    Description:{type:String, required:true},
-     Weight:{type:String, required:true},
-    Icon:{type:String, required:true},
-    Current_Price:{type:Number, required:true}  
+    End_Date: { type: Date, required: true },
+    Description: { type: String, required: true },
+    Weight: { type: String, required: true },
+    Icon: { type: String, required: true },
+    Current_Price: { type: Number, required: true }
 });
 const Promotion = db.model('Promotion', promotionSchema);
 module.exports = Promotion;
