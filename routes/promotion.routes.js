de2ea83eccac15ba2b65af22b55d8c7884b728cd
ThulaniThehara <athukoralaatt.23@uom.lk>
2025-10-13 08:38:40 +0530

    promotion controller model routes update

--- routes/promotion.routes.js
+++ routes/promotion.routes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const promotionController = require("../controllers/promotion.controller");
 const router = express.Router();
+const upload = require("../middleware/upload");
 
-
-router.post('/add', promotionController.addPromotion);
+const multer = require('multer');
+router.post('/add', upload.single('icon'), promotionController.addPromotion);
 router.get('/get', promotionController.getPromotion);
 router.get('/get/:id', promotionController.getPromotionbyId);
 router.get('/get/name/:name', promotionController.getPromotionbyName);
-router.get('/get/type/:type', promotionController.getPromotionbyType);
-router.get('/get/startDate/:date', promotionController.getPromotionbyStartDate);
 router.get('/get/endDate/:date', promotionController.getPromotionbyEndDate);
-router.put('/replace/:id', promotionController.replacePromotion);
-router.patch('/update/:id', promotionController.updatePromotion);
+router.patch('/update/:id', upload.single('icon'), promotionController.updatePromotion);
 router.delete('/delete/:id', promotionController.deletePromotion);
 
+
 module.exports = router;
+
 
